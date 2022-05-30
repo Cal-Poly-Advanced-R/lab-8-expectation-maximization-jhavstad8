e95e4f6fdb17acb1a56756abef8ec53b3e6574a6
jhavstad8 <jhavstad@calpoly.edu>
2022-05-29 19:42:59 -0700

    Update em_clust.R

---
+++ R/em_clust.R
@@ -0,0 +1,17 @@
+#' Perform the Expectation-Maximization Algorithm on a dataset
+#'
+#'
+#' @param dat dataset to perform the algorithm on
+#' @param k The number of random observatoins to start with
+#'
+#' @return A list with cluster assignments and parameter estimates
+#'
+#' @examples
+#'
+#' @import tidyverse
+#'
+#' @export
+
+em_clust <- function(dat,k){
+
+}
