7d0c0fb39b75d4b01ef671897cfe9d82ed12a93c
jhavstad8 <jhavstad@calpoly.edu>
2022-05-31 15:00:17 -0700

    updates

--- R/em_clust.R
+++ R/em_clust.R
@@ -10,6 +10,7 @@
 #'
 #' @import tidyverse
 #' @import mvtnorm
+#' @import stats
 #' @export
 
 em_clust <- function(dat,k){
@@ -18,7 +19,7 @@ em_clust <- function(dat,k){
     cv_mat <- cov(dat)
     probs <- c()
     for(i in 1:k){
-    prob <- dmvnorm(x = dat, mean = as.numeric(est_means[k,]), sigma = cv_mat)
+    prob <- dmvnorm(x = dat, mean = as.numeric(est_means[i,]), sigma = cv_mat)
     probs <- cbind(probs,prob)
     }
     # find max prob for each observation
@@ -28,24 +29,29 @@ em_clust <- function(dat,k){
     dat2 <- cbind(dat, c_vect)
     # m step
     # find cluster means
-    c_means <- aggregate(dat2[,1:ncol(dat)], list(dat2$c_vect, FUN = mean))
+    c_means <- aggregate(dat2[,1:ncol(dat)], list(dat2$c_vect), FUN = mean)
     # split up dataset by cluster
     clusters <- split(x = dat2, f = c_vect)
-    for(i in 1:20){
+
+    old_means <- 0
+    new_means <- 1
+    while(are_equal(old_means,new_means) == F){
     new_probs = c()
-    for(i in 1:k){
-        new_dat <- clusters[i]
+    old_means <- new_means
+    for(j in 1:k){
+        new_dat <- clusters[j] %>% as.data.frame()
         new_dat <- new_dat[,1:ncol(dat)]
-        new_mean <- c_means[i,]
+        new_mean <- c_means[j,]
         new_mean <- new_mean[,2:ncol(new_mean)]
-        update_prob <- dmvnorm(x = new_dat, mean = as.numeric(new_mean), sigma = cov(new_dat))
+        update_prob <- dmvnorm(x = dat, mean = as.numeric(new_mean), sigma = cov(new_dat))
         new_probs <- cbind(new_probs,update_prob)
     }
     # repeat same steps previously
     max_p <- apply(new_probs,1,which.max)
     c_vect <- as.numeric(max_p)
     dat2 <- cbind(dat, c_vect)
-    c_means <- aggregate(dat2[,1:ncol(dat)], list(dat2$c_vect, FUN = mean))
+    c_means <- aggregate(dat2[,1:ncol(dat)], list(dat2$c_vect), FUN = mean)
+    new_means <- c_means
     clusters <- split(x = dat2, f = c_vect)
     }
     return(c_vect)
